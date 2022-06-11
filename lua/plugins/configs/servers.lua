b4046fd98f342aec00cc7963607c7e0801779cac
Sean Ye <seanye24@gmail.com>
2022-06-10 21:40:46 -0700

    add single file mode for tsserver

--- lua/plugins/configs/servers.lua
+++ lua/plugins/configs/servers.lua
@@ -1,24 +1,28 @@
 return {
-  sumneko_lua = {
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { "vim" },
-        },
-        workspace = {
-          library = {
-            [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-            [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-          },
-          maxPreload = 100000,
-          preloadFileSize = 10000,
-        },
+   sumneko_lua = {
+      settings = {
+         Lua = {
+            diagnostics = {
+               globals = { "vim" },
+            },
+            workspace = {
+               library = {
+                  [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+                  [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+               },
+               maxPreload = 100000,
+               preloadFileSize = 10000,
+            },
+         },
       },
-    },
-  },
-  pyright = {},
-  tsserver = {},
-  gopls = {},
-  clangd = {},
-  jdtls = {},
+   },
+   pyright = {},
+   tsserver = {
+      root_dir = function(fname)
+         return require("lspconfig.server_configurations.tsserver").default_config.root_dir(fname) or vim.fn.getcwd()
+      end,
+   },
+   gopls = {},
+   clangd = {},
+   jdtls = {},
 }
